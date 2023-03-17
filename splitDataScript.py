93a342dfc944d2f0041974998f1046bd3759e09c
jonah0502 <jonah0502@gmail.com>
2023-03-16 22:36:47 -0700

    updated to contain clis for test and percentage

--- splitDataScript.py
+++ splitDataScript.py
@@ -14,13 +14,8 @@ import shutil
 #   This program will randomly assign images to your training and validation set in your current directory
 
 #   Run this program with
-#   python splitDataScript.py path/to/parent_dir
+#   python splitDataScript.py path/to/parent_dir --test-true --percentage percent<-(range 0-1)
 
-
-#   Check if theres the right amount of commandline argument
-if(len(sys.argv) != 2):
-    print("Please use a existing directory name as a first argument")
-    exit()
 #   Check if the input directory exists
 if(not os.path.exists(sys.argv[1])):
     print("That directory does not exist")
@@ -28,7 +23,9 @@ if(not os.path.exists(sys.argv[1])):
 
 trainPath = 'train' + sys.argv[1]
 valPath = 'val' + sys.argv[1]
-
+testPath = 'test' + sys.argv[1]
+isTest = False
+isPercent = False
 #   Check if train/val directories already exist
 if(os.path.exists(trainPath) or os.path.exists(valPath)):
     print("Test or validation directory already exists")
@@ -37,7 +34,20 @@ if(os.path.exists(trainPath) or os.path.exists(valPath)):
 #   Make the train and test directories
 os.mkdir(trainPath)
 os.mkdir(valPath)
-
+percentage = 1
+if (sys.argv[2] == '--test-true'):
+    isTest = True
+    os.mkdir(testPath)
+    os.mkdir(testPath + '/images/')
+    os.mkdir(testPath + '/labels/')
+    if len(sys.argv) >= 4:
+        print(len(sys.argv))
+        if sys.argv[3] == '--percentage':
+            percentage = sys.argv[4]
+            isPercent = True
+elif (sys.argv[2] == '--percentage'):
+    percentage = sys.argv[3]
+    isPercent = True
 #   Make the subdirectories of each
 os.mkdir(trainPath + '/images/')
 os.mkdir(trainPath + '/labels/')
@@ -47,15 +57,16 @@ os.mkdir(valPath + '/labels/')
 #   Get the file names of the original directory
 fNames = os.listdir(sys.argv[1] + '/images/')
 
+newLen = round(len(fNames) * float(percentage))
 #   Remove .jpg from each file name
-for i in range(len(fNames)):
+for i in range(newLen):
     fNames[i] = fNames[i][0:(len(fNames[i]) - 4)]
 
 #   shuffle order and segment
 random.shuffle(fNames)
 #   70% goes into training
-trainFNames = fNames[:((7 * len(fNames)) // 10)]
-valFNames = fNames[((7 * len(fNames)) // 10):]
+trainFNames = fNames[:((7 * newLen) // 10)]
+valFNames = fNames[((7 * newLen) // 10):]
 
 trainData = 0
 valData = 0
@@ -67,6 +78,10 @@ for name in trainFNames:
     if(os.path.exists(srcImageFName + '.jpg') and os.path.exists(srcLabelFName + '.txt')):
         shutil.copyfile(srcImageFName + '.jpg', trainPath + '/images/' + name + '.jpg')
         shutil.copyfile(srcLabelFName + '.txt', trainPath + '/labels/' + name + '.txt')
+        if (isTest):
+            if(not isPercent and trainData < (len(fNames) * 0.1)):
+                shutil.copyfile(srcImageFName + '.jpg', testPath + '/images/' + name + '.jpg')
+                shutil.copyfile(srcLabelFName + '.txt', testPath + '/labels/' + name + '.txt')
         trainData += 1
     else:
         print('skipped one file not found in original dataset')
